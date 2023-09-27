006171d2eeef612a7d68f15c3c3e62b5a56222de
Fusn126 <fushen@utry.cn>
2023-09-27 16:50:00 +0800

    feat: 新增变更检测的例子

--- angular-app/src/app/pages/detail/detail.component.ts
+++ angular-app/src/app/pages/detail/detail.component.ts
@@ -1,15 +1,36 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, KeyValueDiffers } from '@angular/core';
 
 @Component({
   selector: 'app-detail',
   templateUrl: './detail.component.html',
-  styleUrls: ['./detail.component.css']
+  styleUrls: ['./detail.component.css'],
 })
 export class DetailComponent implements OnInit {
+  public data = {
+    value: 123,
+  };
+  public processedData = {
+    value: 456,
+  };
+  private differ: any;
 
-  constructor() { }
+  constructor(private differs: KeyValueDiffers) {}
 
   ngOnInit() {
+    this.differ = this.differs.find(this.data).create();
   }
 
+  ngDoCheck(): void {
+    const changes = this.differ.diff(this.data);
+
+    if (changes) {
+      console.log('Changes detected!');
+      this.processedData.value = this.data.value;
+      // Handle changes here
+    }
+  }
+
+  public clickButton(): void {
+    this.data.value++;
+  }
 }
